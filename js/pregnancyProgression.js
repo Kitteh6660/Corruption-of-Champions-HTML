f90ca1bb4cd31b11adedb669cd6e055f154c666c
mattibun <mattibun@gmail.com>
2016-07-02 00:15:28 -0400

    Quick file test.

--- js/pregnancyProgression.js
+++ js/pregnancyProgression.js
@@ -1,19 +1,13 @@
 /*
- This file contains the detailed code for handling player pregnancy, heat, and other things. pregnancy.js will still count down the pregnancy. It basically runs of a huge advanceTime function, at least for now.
+ This file contains the detailed code for handling player pregnancy, heat, and other things. pregnancy.js will still count down the pregnancy.
  */
 
-pregnancyProgression = [];
+var pregnancyProgression = [];
 
 pregnancyProgression.pregProg = function() {
 return;
 };
 
-pregnancyProgresssion.pregProg.prototype.advanceTime = new function() {
-        outputText("Checks for player strangeness for pregnancy.");
-
-        
-
- };
-
-timeAware.push(pregnancyProgression.pregProg);
-
+pregnancyProgression.updatePregnancy = function () {
+return;
+};
