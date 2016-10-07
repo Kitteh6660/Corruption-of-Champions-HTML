b9577a4eae3934c4720cda3bb484df45da4af016
Mattibun <mattibun@gmail.com>
2016-10-07 15:40:02 -0400

    Trying this again...

--- js/scenes/combat.js
+++ js/scenes/combat.js
@@ -26,9 +26,7 @@ battleMenu = function() {
 	addButton(4, "Run", flee);
     addButton(5, "P. Specials", physicalSpecials);
     addButton(6, "M. Specials", mentalSpecials);
-    // Making change so buttons make sense for Sand Trap
-    if (monster.refName = "sandtrap") { addButton(7, "Climb", wait); }
-    else { addButton(7, "Wait", wait); }
+    addButton(7, "Wait", wait);
     addButton(8, "Fantasize", fantasize);
     if (player.findStatusEffect(StatusEffects.Bind) >= 0) {
         menu();
